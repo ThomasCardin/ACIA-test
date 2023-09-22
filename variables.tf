ee2b7afe8fb6bbb11d4028e7f2a974f3bac7c5b6
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2023-09-22 10:31:55 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,25 @@
 # Variables
 variable "postgres_db" {
   description = "Postgres db."
+  default     = "postgres_database"
 }
 
 variable "postgres_user" {
   description = "Postgres user."
+  default     = "postgres_user"
 }
 
 variable "postgres_password" {
   description = "Postgres password."
+  default     = "postgres_password"
 }
 
 variable "postgres_host" {
   description = "Postgres host"
+  default     = "postgres_host"
 }
 
 variable "user_ialab_password" {
   description = "ialab user password"
+  default     = "ialab_user_password"
 }
